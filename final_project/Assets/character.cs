c841abffeec3b7726c07049021b5d96004a66af4
victorgloria <victor.gloria@hotmail.com>
2023-04-06 03:37:12 -0500

    more Animation

--- final_project/Assets/character.cs
+++ final_project/Assets/character.cs
@@ -6,10 +6,13 @@ public class character : MonoBehaviour
 {
     Rigidbody2D rb;
 
-    public float speed = 5f;
+    private Animator anim;
+
+    public float Speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -21,6 +24,14 @@ public class character : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.MovePosition(transform.position + new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * Time.fixedDeltaTime * speed);
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        if(moveInput == 0){
+            anim.SetBool("CharMove",false);
+        }
+        else{
+            anim.SetBool("CharMove",true);
+        }
+        rb.MovePosition(transform.position + new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * Time.fixedDeltaTime * Speed);
+
     }
 }
