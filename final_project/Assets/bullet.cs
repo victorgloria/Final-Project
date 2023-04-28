63bbfa144cec23e680fee4239269d1b344168911
victorgloria <victor.gloria@hotmail.com>
2023-04-28 03:45:32 -0500

    just working on the shooting

--- final_project/Assets/bullet.cs
+++ final_project/Assets/bullet.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class bullet : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed = 10f;
+    private Vector3 mousePos;
+    private Camera mainCam;
     private Rigidbody2D rb;
+    public float speed;
     void Start()
     {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * speed;
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x,direction.y).normalized * speed;
+        float rot = Mathf.Atan2(rotation.y,rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,rot);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "wall"){
+        Destroy(gameObject);
+        }
     }
 }
--- final_project/Assets/shootingDirection.cs
+++ final_project/Assets/shootingDirection.cs
@@ -6,6 +6,12 @@ public class shootingDirection : MonoBehaviour
 {
     private Camera mainCam;
     private Vector3 mousePos;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +25,18 @@ public class shootingDirection : MonoBehaviour
         Vector3 rotation = mousePos - transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0,0,rotZ);
+        if(!canFire){
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring){
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if(Input.GetMouseButton(0) && canFire){
+            canFire = false;
+            Instantiate(bullet,bulletTransform.position, Quaternion.identity);
+
+        }
     }
 }
