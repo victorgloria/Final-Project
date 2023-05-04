a56fd3330060a752588ce1057548b569c393d9ec
victorgloria <victor.gloria@hotmail.com>
2023-05-03 22:38:20 -0500

    main menu

---
+++ final_project/Assets/mainmenuhandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenuhandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+        public void startGame(){
+        SceneManager.LoadScene("Level1");
+    }
+    // Update is called once per frame
+    public void exitGame(){
+        Application.Quit();
+    }
+}
